1b89a964c88b2fd9c1ecfbb3a186598e6cce812c
Scott Regenthal <scottregenthal@gmail.com>
2019-11-02 10:04:22 -0600

    Add course model method test for ordering  student grades descending

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -9,4 +9,20 @@ RSpec.describe Course, type: :model do
     it { should have_many :course_students }
     it { should have_many(:students).through(:course_students) }
   end
+
+  describe 'instance methods' do
+    it 'rank_grades' do
+      art = Course.create!(name: 'Art')
+
+      bob = Student.create!(name: 'Bob')
+      dan = Student.create!(name: 'Dan')
+      lou = Student.create!(name: 'Lou')
+
+      art_bob = CourseStudent.create!(course: art, student: bob, grade: 3.4)
+      art_dan = CourseStudent.create!(course: art, student: dan, grade: 3.8)
+      art_lou = CourseStudent.create!(course: art, student: lou, grade: 3.6)
+
+      expect(art.rank_grades).to eq([dan, lou, bob])
+    end
+  end
 end
