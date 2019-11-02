8a96a1e5b65f3f2412b4a1cd216b91baa1958f54
Scott Regenthal <scottregenthal@gmail.com>
2019-11-01 21:18:38 -0600

    Create student controller with show action, route and view

--- spec/features/students/show_spec.rb
+++ spec/features/students/show_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'Student Show Page' do
     art = Course.create!(name: 'Art')
     math = Course.create!(name: 'Music')
 
-    bob = Student.create!(name: 'Tom')
+    bob = Student.create!(name: 'Bob')
 
     art_bob = CourseStudent.create!(course: art, student: bob, grade: 3.8)
     math_bob = CourseStudent.create!(course: math, student: bob, grade: 3.6)
@@ -14,12 +14,12 @@ RSpec.describe 'Student Show Page' do
 
     expect(page).to have_content(bob.name)
 
-    within "course-#{art.id}" do
+    within "#course-#{art.id}" do
       expect(page).to have_content(art.name)
       expect(page).to have_content(art_bob.grade)
     end
 
-    within "course-#{math.id}" do
+    within "#course-#{math.id}" do
       expect(page).to have_content(math.name)
       expect(page).to have_content(math_bob.grade)
     end
