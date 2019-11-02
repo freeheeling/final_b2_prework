0d1d048c1ceb19dd47417449fef0d092c8c42fbe
Scott Regenthal <scottregenthal@gmail.com>
2019-11-02 09:58:57 -0600

    Modify test and view to include content descriptors and path helper

--- spec/features/students/show_spec.rb
+++ spec/features/students/show_spec.rb
@@ -10,18 +10,18 @@ RSpec.describe 'Student Show Page' do
     art_bob = CourseStudent.create!(course: art, student: bob, grade: 3.8)
     math_bob = CourseStudent.create!(course: math, student: bob, grade: 3.6)
 
-    visit "/students/#{bob.id}"
+    visit student_path(bob.id)
 
-    expect(page).to have_content(bob.name)
+    expect(page).to have_content("#{bob.name}: Enrolled Courses and Grades")
 
     within "#course-#{art.id}" do
-      expect(page).to have_content(art.name)
-      expect(page).to have_content(art_bob.grade)
+      expect(page).to have_content("Course: #{art.name}")
+      expect(page).to have_content("Grade: #{art_bob.grade}")
     end
 
     within "#course-#{math.id}" do
-      expect(page).to have_content(math.name)
-      expect(page).to have_content(math_bob.grade)
+      expect(page).to have_content("Course: #{math.name}")
+      expect(page).to have_content("Grade: #{math_bob.grade}")
     end
   end
 end
