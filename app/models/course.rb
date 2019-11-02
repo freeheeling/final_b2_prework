8ede3e9146a25d93678eddbe7b178bf258a5f8cd
Scott Regenthal <scottregenthal@gmail.com>
2019-11-02 10:07:10 -0600

    Create CourseController and view for show action, and add rank_grades method to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@ class Course < ApplicationRecord
 
   has_many :course_students
   has_many :students, through: :course_students
+
+  def rank_grades
+    students.joins(:course_students).order('course_students.grade desc')
+  end
 end
