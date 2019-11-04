46b95f368f8b25a25aca37d3bdf916922c0c2496
Scott Regenthal <scottregenthal@gmail.com>
2019-11-04 07:11:41 -0700

    Update course model, view and spec for grade order method via course_students

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,7 +4,7 @@ class Course < ApplicationRecord
   has_many :course_students
   has_many :students, through: :course_students
 
-  def rank_grades
-    students.joins(:course_students).order('course_students.grade desc')
+  def order_grades
+    course_students.order(grade: :desc)
   end
 end
