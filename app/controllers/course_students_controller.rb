73d7f6f8c2bad55288baa91698792951c0519eca
Scott Regenthal <scottregenthal@gmail.com>
2019-11-05 10:49:11 -0700

    Begin to develop create action wihin CourseStudents controller

--- app/controllers/course_students_controller.rb
+++ app/controllers/course_students_controller.rb
@@ -1,5 +1,12 @@
 class CourseStudentsController < ApplicationController
 
+  def create
+    Course.create(course_params)
+    Student.create(student_params)
+    course_student = CourseStudent.create(course_student_params)
+    redirect_to student_path(course_student.student.id)
+  end
+
   def destroy
     course_student = CourseStudent.find_by(course_student_params)
     course_student.destroy
@@ -9,5 +16,13 @@ class CourseStudentsController < ApplicationController
   private
     def course_student_params
       params.permit(:course_id, :student_id, :grade)
+    end
+
+    def course_params
+      params.permit(:name)
+    end
+
+    def student_params
+      params.permit(:name)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
   resources :courses, only: [:show]
 
+  post '/courses/:id', to: 'course_students#create'
   delete '/courses/:id', to: 'course_students#destroy'
 end
