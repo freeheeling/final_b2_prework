a68c444bd760e7a4f8cffd53befc4a612599ee81
Scott Regenthal <scottregenthal@gmail.com>
2019-11-05 09:11:00 -0700

    Add test and unenroll button within each student id section on course show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :students, only: [:show]
+
   resources :courses, only: [:show]
 end
--- spec/features/courses/show_spec.rb
+++ spec/features/courses/show_spec.rb
@@ -31,4 +31,34 @@ RSpec.describe 'Course Show Page' do
       expect(page).to have_content("Grade: #{art_bob.grade}")
     end
   end
+
+  it 'displays a button to unenroll a student next to their name' do
+    art = Course.create!(name: 'Art')
+
+    bob = Student.create!(name: 'Bob')
+    dan = Student.create!(name: 'Dan')
+
+    art_bob = CourseStudent.create!(course: art, student: bob, grade: 3.4)
+    art_dan = CourseStudent.create!(course: art, student: dan, grade: 3.8)
+
+    visit course_path(art.id)
+
+    within "#student-#{dan.id}" do
+      expect(page).to have_content("Student: #{dan.name}")
+      expect(page).to have_content("Grade: #{art_dan.grade}")
+      expect(page).to have_button('Unenroll')
+    end
+
+    within "#student-#{bob.id}" do
+      expect(page).to have_content("Student: #{bob.name}")
+      expect(page).to have_content("Grade: #{art_bob.grade}")
+      expect(page).to have_button('Unenroll')
+    end
+  end
 end
+
+# As a user,
+# When I visit a course show page,
+# Then I see a button 'unenroll' next to each student's name
+# When I click this button,
+# Then I am redirected to that student's show page where I do not see the course listed
