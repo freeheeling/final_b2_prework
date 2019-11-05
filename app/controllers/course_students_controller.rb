448515a00a84a2c6a10588a5130c23501774fe80
Scott Regenthal <scottregenthal@gmail.com>
2019-11-05 10:05:51 -0700

    Add strong params for destroy action in CourseStudents controller

--- app/controllers/course_students_controller.rb
+++ app/controllers/course_students_controller.rb
@@ -1,7 +1,13 @@
 class CourseStudentsController < ApplicationController
 
   def destroy
-    CourseStudent.destroy.where('course_id, student_id')
+    course_student = CourseStudent.find_by(course_student_params)
+    course_student.destroy
     redirect_to student_path(course_student.student.id)
   end
+
+  private
+    def course_student_params
+      params.permit(:course_id, :student_id, :grade)
+    end
 end
